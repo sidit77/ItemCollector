1ad588cdae3f9c94461434bc1676608e9e10baf7
sidit77 <sidit77@gmail.com>
2022-05-29 21:21:21 +0200

    working item teleportation

--- src/main/java/com/github/sidit77/item_collector/ItemCollector.java
+++ src/main/java/com/github/sidit77/item_collector/ItemCollector.java
@@ -2,9 +2,17 @@ package com.github.sidit77.item_collector;
 
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.predicate.entity.EntityPredicates;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3d;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
 
 public class ItemCollector implements ModInitializer {
 
@@ -13,10 +21,25 @@ public class ItemCollector implements ModInitializer {
     public static final String MOD_ID = "item_collector";
     public static final String MOD_NAME = "Item Collector";
 
+    public static final Identifier COLLECT_PACKET_ID = new Identifier(MOD_ID, "collect_packet");
 
     @Override
     public void onInitialize() {
         log(Level.INFO, "Initializing..");
+        ServerPlayNetworking.registerGlobalReceiver(COLLECT_PACKET_ID, (server, player, handler, buf, responseSender) -> {
+            log(Level.DEBUG, "Received collect packet");
+            server.execute(() -> {
+                Vec3d playerPos = player.getPos();
+                List<ItemEntity> items = player
+                        .getServerWorld()
+                        .getEntitiesByType(EntityType.ITEM,
+                                player.getBoundingBox().expand(16.0D),
+                                EntityPredicates.VALID_ENTITY);
+                for(ItemEntity item : items) {
+                    item.setPosition(playerPos.x, playerPos.y, playerPos.z);
+                }
+            });
+        });
     }
 
     public static void log(Level level, String message){
--- src/main/java/com/github/sidit77/item_collector/client/ItemCollectorClient.java
+++ src/main/java/com/github/sidit77/item_collector/client/ItemCollectorClient.java
@@ -1,12 +1,17 @@
 package com.github.sidit77.item_collector.client;
 
+import com.github.sidit77.item_collector.ItemCollector;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.text.LiteralText;
+import org.apache.logging.log4j.Level;
 import org.lwjgl.glfw.GLFW;
 
 import static net.fabricmc.api.EnvType.CLIENT;
@@ -20,12 +25,20 @@ public class ItemCollectorClient implements ClientModInitializer {
             GLFW.GLFW_KEY_B,
             "key.categories.gameplay"
     ));
+    private boolean keyDown = false;
 
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (keyBinding.wasPressed()) {
-                client.player.sendMessage(new LiteralText("Key 1 was pressed!"), false);
+            if (keyBinding.isPressed()) {
+                if(!keyDown){
+                    //client.player.sendMessage(new LiteralText("Key 1 was pressed!"), false);
+                    ItemCollector.log(Level.DEBUG, "Pressed collect button");
+                    ClientPlayNetworking.send(ItemCollector.COLLECT_PACKET_ID, PacketByteBufs.empty());
+                    keyDown = true;
+                }
+            } else {
+                keyDown = false;
             }
         });
     }
