1d8b4814fc2fa1aa82c652c535c586f432d893f8
sidit77 <sidit77@gmail.com>
2022-05-29 20:41:31 +0200

    added a keybinding

--- src/main/java/com/github/sidit77/item_collector/client/ItemCollectorClient.java
+++ src/main/java/com/github/sidit77/item_collector/client/ItemCollectorClient.java
@@ -2,14 +2,31 @@ package com.github.sidit77.item_collector.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.LiteralText;
+import org.lwjgl.glfw.GLFW;
 
 import static net.fabricmc.api.EnvType.CLIENT;
 
 @Environment(CLIENT)
 public class ItemCollectorClient implements ClientModInitializer {
 
+    private static final KeyBinding keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.item_collector.collect",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_B,
+            "key.categories.gameplay"
+    ));
+
     @Override
     public void onInitializeClient() {
-
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (keyBinding.wasPressed()) {
+                client.player.sendMessage(new LiteralText("Key 1 was pressed!"), false);
+            }
+        });
     }
 }
